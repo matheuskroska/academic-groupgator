8922a6ec4af0ac2d6143b18486aa9894ec448c59
Matheus Kroska <kroskamatheus@gmail.com>
2021-09-26 22:06:07 -0300

    firebase settings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
+import {useState, useEffect} from "react";
+import firebase from './firebase-config';
+
+
 function App() {
+
+    const [estado, setEstado] = useState();
+    
+    function useUser() {
+      const [usuario, setUser] = useState([]);
+      
+      useEffect(() => {
+        firebase
+          .collection('usuarios')
+          .onSnapshot((snapshot) => {
+            const newUser = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data()
+            }))
+
+            setUser(newUser)
+          })
+      }, [])
+
+      return usuario
+    }
+  
+    const inserirUsuario = async (e) => {
+      e.preventDefault();
+      if(typeof estado != "undefined" || estado != null){
+        await firebase.collection('usuarios').add({
+        nome: estado,
+        insertDate: new Date()
+        }).then( () => {
+          console.log("gravou corretamente");
+        }).catch( (erro) => {
+          console.log("erro " + erro);
+        });
+      }else {
+        alert ("Invalid data");
+      }
+    }
+
+    const listarUsuario = async (e) => {
+      await firebase.collection('usuarios').get()
+      .then((snapshot) => {
+        let lista = [];
+
+        snapshot.forEach((item)=>{
+          lista.push({
+            id: item.id,
+            nome: item.data().nome,
+            insertDate: item.data().insertDate
+          });
+        });
+      })
+    }
+
+    const times = useUser()
+   
   return (
     <div>
-    </div>
+      <button onClick={inserirUsuario}>Inserir Usuario</button><br></br>
+      <input placeholder="valor dinamico" onChange={e => setEstado(e.target.value)}></input>
+      <button onClick={listarUsuario}>Listar Usuario</button>
+      <br></br>
+        {times.length > 0 ? times.map( times => <div key={times.id}><div>{times.nome}</div><div>{times.insertDate.toString()}</div></div>) : <>Não existe registro no banco</>}</div> 
+        // componente com props ou componente de mensagem vazio
   );
 }
-
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+let firebaseConfig =  {
+    apiKey: "AIzaSyDh-f_xr_QLJ0pqBDDq-t1jlNPWhXOIAog",
+    authDomain: "groupgator-ed4db.firebaseapp.com",
+    projectId: "groupgator-ed4db",
+    storageBucket: "groupgator-ed4db.appspot.com",
+    messagingSenderId: "608229845484",
+    appId: "1:608229845484:web:ffd950ca0e82c4fbcaf8c9",
+    measurementId: "G-ZSMHE2ZNJH"
+};
+
+if(!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+export default firebase.firestore();
